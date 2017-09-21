f7c166814b122cd95359c17092f5cfdbff075513
Adel Acebal <adelacebal@yahoo.com>
2017-09-21 16:14:43 -0400

    repository added

--- src/main/java/com/adelacebal/giflib/controller/GifController.java
+++ src/main/java/com/adelacebal/giflib/controller/GifController.java
@@ -1,21 +1,24 @@
 package com.adelacebal.giflib.controller;
 
+import com.adelacebal.giflib.data.GifRepository;
 import com.adelacebal.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
-
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
     }
     @RequestMapping("/gif")
     public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("book-dominos", LocalDate.of(2015, 2, 13), "Adel Acebal", true);
+        Gif gif = gifRepository.findByName("android-explosion");
         modelMap.put("gif", gif);
         return "gif-details";
     }
--- src/main/java/com/adelacebal/giflib/data/GifRepository.java
+++ src/main/java/com/adelacebal/giflib/data/GifRepository.java
@@ -1,4 +1,30 @@
 package com.adelacebal.giflib.data;
 
+import com.adelacebal.giflib.model.Gif;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class GifRepository {
+    private static final List<Gif> ALL_GIFTS = Arrays.asList(
+            new Gif("android-explosion", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("ben-and-mike", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("book-dominos", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("compiler-bot", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("cowboy-coder", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("infinite-andrew", LocalDate.of(2017, 9, 18), "Adel Acebal", true),
+            new Gif("rainbow-giphy", LocalDate.of(2017, 9, 18), "Adel Acebal", true)
+    );
+
+    public Gif findByName(String name) {
+        for (Gif gif : ALL_GIFTS) {
+            if (gif.getName().equals(name)) {
+                return gif;
+            }
+        }
+        return null;
+    }
 }
